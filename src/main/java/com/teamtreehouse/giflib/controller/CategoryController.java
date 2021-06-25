2ad247a5e15c16199d25cce512f5516e95f37221
Kevin Stradtman <kevin.stradtman@gmail.com>
2021-06-25 14:59:42 -0400

    Added CategoryController and CategoryRepository

---
+++ src/main/java/com/teamtreehouse/giflib/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package com.teamtreehouse.giflib.controller;
+
+
+import com.teamtreehouse.giflib.data.CategoryRepository;
+import com.teamtreehouse.giflib.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class CategoryController {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @RequestMapping("/categories")
+    public String listCategories(ModelMap modelMap) {
+        List<Category> categories = categoryRepository.getAllCategories();
+        modelMap.put("categories", categories);
+        return "categories";
+    }
+}
