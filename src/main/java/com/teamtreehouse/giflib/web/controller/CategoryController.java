547eadb05836d1a31dbbabcdb8dbb7822d3e28d5
Kevin Stradtman <kevin.stradtman@gmail.com>
2021-06-26 13:53:45 -0400

    Added FlashMessages to the app upon submitting a category

--- src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
+++ src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
@@ -2,15 +2,20 @@ package com.teamtreehouse.giflib.web.controller;
 
 import com.teamtreehouse.giflib.model.Category;
 import com.teamtreehouse.giflib.service.CategoryService;
+import com.teamtreehouse.giflib.web.Color;
+import com.teamtreehouse.giflib.web.FlashMessage;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,7 +41,10 @@ public class CategoryController {
 
     @RequestMapping("categories/add")
     public String formNewCategory(Model model) {
-
+        if(!model.containsAttribute("category")) {
+            model.addAttribute("category", new Category());
+        }
+        model.addAttribute("colors", Color.values());
         return "category/form";
     }
 
@@ -52,8 +60,14 @@ public class CategoryController {
     }
 
     @RequestMapping(value = "/categories", method = RequestMethod.POST)
-    public String addCategory(Category category) {
+    public String addCategory(@Valid Category category, BindingResult result, RedirectAttributes redirectAttributes) {
+        if(result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.category", result);
+            redirectAttributes.addFlashAttribute("category", category);
+            return "redirect:/categories/add";
+        }
         categoryService.save(category);
+        redirectAttributes.addFlashAttribute("flash", new FlashMessage("Category successfully added", FlashMessage.Status.SUCCESS));
         return "redirect:/categories";
     }
 
