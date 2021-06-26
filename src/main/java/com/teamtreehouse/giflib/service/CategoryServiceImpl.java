b71ab9b6b6853c22f71267c9c6b8769eee9e4220
Kevin Stradtman <kevin.stradtman@gmail.com>
2021-06-26 00:16:16 -0400

    Created the CategoryService and the CategoryServiceImpl

---
+++ src/main/java/com/teamtreehouse/giflib/service/CategoryServiceImpl.java
@@ -0,0 +1,34 @@
+package com.teamtreehouse.giflib.service;
+
+import com.teamtreehouse.giflib.dao.CategoryDAO;
+import com.teamtreehouse.giflib.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    private CategoryDAO categoryDAO;
+
+    @Override
+    public List<Category> findAll() {
+        return categoryDAO.findAll();
+    }
+
+    @Override
+    public Category findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(Category category) {
+
+    }
+
+    @Override
+    public void delete(Category category) {
+
+    }
+}
